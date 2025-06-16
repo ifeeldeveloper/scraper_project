ecb1fe4f1d27602bda62e710d2aa80edc488aa58
Jagadish Bhatta <jagadishbhatta16@gmail.com>
2025-06-16 11:12:57 +0545

    Project completion

---
+++ scrape.py
@@ -0,0 +1,50 @@
+# python -m pip install requests
+# => get data from web (html, json, xml)
+# python -m pip install beautifulsoup4
+# parse html
+
+import requests
+
+from bs4 import BeautifulSoup
+
+import json
+
+# import pprint
+
+url = "http://books.toscrape.com/"
+
+def scrape_book(url):
+  response = requests.get(url)
+  response.encoding = response.apparent_encoding
+  if response.status_code != 200:
+    return
+  
+  soup = BeautifulSoup(response.text, "html.parser")
+  books = soup.find_all("article", class_ ="product_pod")
+
+  book_data = []
+
+  for book in books:
+    title = book.h3.a["title"]
+    price_text = book.find("p", class_ = "price_color").text
+
+    currency = price_text[0]
+    price = float(price_text[1:])
+
+    book_data.append({
+      'title': title,
+      'currency': currency,
+      'price': price
+    })
+  
+  return book_data
+
+books = scrape_book(url)    
+
+
+with open("data.json", 'w', encoding= 'utf-8') as f:
+  json.dump(books, f, indent=2, ensure_ascii=False)
+  
+print("Book data saved to data.json")
+
+# pprint.pprint(books)
